fc3fe039c4d9e8bcb71b3c9c7dd6007c70802a8e
martflorin <mart.florin@yahoo.com>
2018-05-30 15:13:32 +0300

    register-validation

--- StoW/controllers/register.php
+++ StoW/controllers/register.php
@@ -1,7 +1,7 @@
 <?php
 
 class Register extends Controller {
-
+    public $variabila="";
 	function __construct() {
 		parent::__construct();	
 	}
@@ -9,6 +9,24 @@ class Register extends Controller {
 	function index() 
 	{	
 		$this->view->render('register/index');
+	}
+	function checkValidity($data){
+		if(empty($data['login']) || empty($data['password']) || empty($data['nume'])|| empty($data['prenume'])|| empty($data['varsta'])|| empty($data['email'])){
+			echo 'Campuri goale.';
+			$variabila = "ceva";
+			return 0;
+		}
+		if (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
+			$error = "Email invalid";
+			echo $error;
+			return 0;
+		}
+		if(!is_numeric($data['varsta'])){
+			$error = "Varsta nu este in formatul care trebuie";
+			echo $error;
+			return 0;
+		}
+		return 1;
 	}
 	public function create() 
 	{
@@ -22,15 +40,7 @@ class Register extends Controller {
 		$data['varsta'] = $_POST['varsta'];
 		$data['email'] = $_POST['email'];
 
-		if (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
-  			$emailErr = "Invalid email format"; 
-  			echo $emailErr;
-
-		}
-		else{
-		
-		// @TODO: Do your error checking!
-		
+		if($this->checkValidity($data)){
 		$this->model->create($data);
 		header('location: ' . URL . 'login');
 		}
