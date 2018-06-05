5406fbbe78868627f2eba948a37b0db8ac5a63bc
stefan-adrian <stefan_dx97@yahoo.com>
2018-06-05 16:23:18 +0300

    Fixes

--- StoW/views/book_detail/index.php
+++ StoW/views/book_detail/index.php
@@ -6,34 +6,19 @@ $thisBook=Session::get('thisBook');
 
 
     <div >
-<<<<<<< HEAD
-        <div class="rightMenu">
 
+        
 
-            <div class="bookInfo">
-                <div class="bookCover" style="background-image:url(public/images/Origin.jpg)">
-                
-                </div>
-                <div class="bookTitle">
-                    <br>
-                    <b>Titlu:</b> <a href="<?php echo URL; ?>read?page=<?php 1 //aici va trebui adaugat bookmarkul ?>">   <?php echo $thisBook->getName()  ?>  </a>
-                    
-                    
-                    <br/>
-                    
-                </div>
-                <div class="bookSummary">
-                </div>
-=======
         
         <div class="title">
-            <b>Title:</b>   <?php echo $thisBook->getName()  ?>
+            <b>Title:</b>  <a href="<?php echo URL; ?>read?page=<?php 1 //aici va trebui adaugat bookmarkul ?>" style=color:blue;>   <?php echo $thisBook->getName()  ?>   </a>
             <br/>
             <b>Author:</b>  <?php echo $thisBook->getAuthor() ?>
         </div>
         
-        <div class="cover" style="background-image:url(<?php echo $thisBook->getPhotoLink(); ?>);">
-            Photo - Not working
+        <div class="cover" >
+            
+            <img src="<?php echo $thisBook->getPhotoLink(); ?>" alt="Italian Trulli">
         </div> 
     
         <div class="description">
@@ -41,7 +26,7 @@ $thisBook=Session::get('thisBook');
             <br><br/>
             <b>Threads:</b>   <?php echo $thisBook->getThread()  ?>
             <br><br/>
->>>>>>> c828b4fd03041b66ff77fa08a74bd6093002a78a
+
             
             <b>Description:</b>   to be added
             <br><br/>
