c828b4fd03041b66ff77fa08a74bd6093002a78a
Razdran <anton.razvan1997@gmail.com>
2018-06-05 11:55:33 +0300

    Book_detail style fixed

--- StoW/views/book_detail/index.php
+++ StoW/views/book_detail/index.php
@@ -6,24 +6,27 @@ $thisBook=Session::get('thisBook');
 
 
     <div >
-        <div class="rightMenu">
-
-
-            <div class="bookInfo">
-                <div class="bookCover" style="background-image:url(images/Origin.jpg)">
-                
-                </div>
-                <div class="bookTitle">
-                    <br>
-                    <b>Titlu:</b>   <?php echo $thisBook->getName()  ?>
-                    <br/>
-                    
-                </div>
-                <div class="bookSummary">
-                </div>
-            
-            </div> 
+        
+        <div class="title">
+            <b>Title:</b>   <?php echo $thisBook->getName()  ?>
+            <br/>
+            <b>Author:</b>  <?php echo $thisBook->getAuthor() ?>
         </div>
+        
+        <div class="cover" style="background-image:url(<?php echo $thisBook->getPhotoLink(); ?>);">
+            Photo - Not working
+        </div> 
+    
+        <div class="description">
+            <b>Year:</b>   <?php echo $thisBook->getYear()  ?>
+            <br><br/>
+            <b>Threads:</b>   <?php echo $thisBook->getThread()  ?>
+            <br><br/>
+            
+            <b>Description:</b>   to be added
+            <br><br/>
+        </div>
+             
     </div>
 
 
