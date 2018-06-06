af086538f9aa49c879cd7d932a612591f8f873fc
Razdran <anton.razvan1997@gmail.com>
2018-06-06 20:03:50 +0300

    Text-to-Speech changes

--- StoW/views/read/index.php
+++ StoW/views/read/index.php
@@ -3,23 +3,9 @@ Session::init();
 $pageText=Session::get('pageText');
 $page=Session::get('page');
 $numberOfPages=Session::get('numberOfPages');
-if($_POST){
-//get the text
-$text = substr($_POST['txttext'], 0, 100);
- 
-//we are passing as a query string so encode it, space will become +
-$text = urlencode($text);
- 
-//give a file name and path to store the file
-$file  = 'filename';
-$file = $file . ".mp3";
- 
-//now get the content from the Google API using file_get_contents
-$mp3 = file_get_contents("http://translate.google.com/translate_tts?tl=en&q=$text");
- 
-//save the mp3 file to the path
-file_put_contents($file, $mp3);
-}
+include 'classes/PHP_Text2Speech.php';
+
+$t2s = new PHP_Text2Speech;
 ?>
 
 <div> 
@@ -51,23 +37,8 @@ file_put_contents($file, $mp3);
     <a href="#" class="next">Page <?php echo ($page)?> of <?php echo ($numberOfPages)?></a>  <br/>
     
     </div>
-    
-    <h2>Text to Speech PHP Script</h2>
- 
-<form action="" method="post">
-Enter your text:
-<textarea name="txttext" rows="5" cols="30"></textarea>
-<br>
-<input type="submit" name="submit" value="Convert">
-</form>
- 
-<?php  if($_POST){?>
- 
-<!-- play the audio file using a player. Here I'm used a HTML5 player. You can use any player insted-->
-<audio controls="controls" autoplay="autoplay">
-<source src="<?php echo $file; ?>" type="audio/mp3" />
+
+ <audio controls="controls" autoplay="autoplay">
+  <source src="<?php echo $t2s->speak('What are you looking at? Wipe that face off your head.'); ?>" type="audio/mp3" />
 </audio>
- 
-<?php }?>
- 
 </div>
