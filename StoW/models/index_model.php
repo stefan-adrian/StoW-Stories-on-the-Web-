a5e846a9531350f6a5dc0525f8b98d23b44dfc5f
Razdran <anton.razvan1997@gmail.com>
2018-06-07 21:37:34 +0300

    Prevent sql injection on index_ model

--- StoW/models/index_model.php
+++ StoW/models/index_model.php
@@ -13,8 +13,14 @@ class Index_Model extends Model
 		Session::init();
 		$me=Session::get('me');
 		$age=$me->getAge();
-		$sth = $this->db->prepare("SELECT * FROM books where ageCategory<=$age");
+		$sth = $this->db->prepare("SELECT * FROM books where ageCategory<= :age");
 
+                
+		$sth->execute(array(
+			':age' => $age
+		));
+
+                
 		$sth->execute();
 		$data=$sth->fetchAll();
 		Session::set('bookList',$data);
@@ -24,9 +30,15 @@ class Index_Model extends Model
 		Session::init();
 		$me=Session::get('me');
 		$age=$me->getAge();
-		$sth = $this->db->prepare("SELECT *  FROM books where name like '%".$_POST['cauta']."%' and ageCategory<=$age ");
-		$sth->execute();
-		$data= $sth->fetchAll();
+		$sth = $this->db->prepare("SELECT *  FROM books where name like '%".$_POST['cauta']."%' and ageCategory<=:age ");
+                
+		$sth->execute(array(
+			':age' => $age
+		));
+
+                
+                
+                $data= $sth->fetchAll();
 		Session::set('bookList2',$data);
 		Session::set('search',1);
     }
